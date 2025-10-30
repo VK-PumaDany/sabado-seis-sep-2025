0cc9afd7b79efda72df196931f3c0c6cb8041f20
VK-PumaDany <vakanodan@gmail.com>
2025-10-30 14:56:54 -0500

    feat: add cors and kernl midelware

---
+++ config/cors.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['https://sabado-seis-sep-2025.onrender.com'], // ⚠️ Cambia esto por tu dominio cuando lo tengas
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
